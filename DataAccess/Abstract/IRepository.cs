31a4a2138889f8e69c56c74a549c322644a2b65a
serhat24 <51510588+serhat24@users.noreply.github.com>
2020-08-29 15:20:53 +0200

    Repository writed

--- DataAccess/Abstract/IRepository.cs
+++ DataAccess/Abstract/IRepository.cs
@@ -4,13 +4,17 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IRepository<TEntity>
+    public interface IRepository<TEntity> where TEntity:class
     {
-        TEntity GetEntity();
-        List<TEntity> GetEntities();
-        void AddEntity();
-        void AddAllEntities();
-        
+        TEntity GetEntityById(int id);
+        IEnumerable<TEntity> GetEntities();
+
+        bool AddEntity(TEntity entity);
+        bool AddRangeEntities(IEnumerable<TEntity> entities);
+
+        bool DeleteEntity(TEntity entity);
+        bool DeleteEntityById(int id);
+        bool DeleteRangeEntity(IEnumerable<TEntity> entities);
 
     }
 }
---
+++ DataAccess/Concrete/Repository.cs
@@ -0,0 +1,108 @@
+﻿using DataAccess.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected DbContext _context;
+        public Repository(DbContext context)
+        {
+            _context = context;
+        }
+        public bool AddEntity(TEntity entity)
+        {
+            try
+            {
+                _context.Set<TEntity>().Add(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+            
+        }
+
+        public bool AddRangeEntities(IEnumerable<TEntity> entities)
+        {
+            try
+            {
+                _context.Set<TEntity>().AddRange(entities);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+            
+           
+        }
+
+        public bool DeleteEntity(TEntity entity)
+        {
+            try
+            {
+                _context.Set<TEntity>().Remove(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+            
+        }
+
+        public bool DeleteEntityById(int id)
+        {
+            var entity=_context.Find<TEntity>(id);
+
+            try
+            {
+                if (entity != null)
+                {
+                    _context.Set<TEntity>().Remove(entity);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+            
+
+        }
+
+        public bool DeleteRangeEntity(IEnumerable<TEntity> entities)
+        {
+            try
+            {
+                _context.Set<TEntity>().RemoveRange(entities);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<TEntity> GetEntities()
+        {
+           return _context.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetEntityById(int id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+    }
+}
